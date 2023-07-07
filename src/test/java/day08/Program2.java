61f6f0c7a5e34261afa3c964d434137fb6b25f1f
Avinash Krishnamoorthy <saiavinashkrishnamoorthy@gmail.com>
2023-07-07 09:03:20 +0530

    waits

--- src/test/java/day08/Program2.java
+++ src/test/java/day08/Program2.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Program01 {
+public class Program2 {
 
 	public static void main(String[] args) {
 		WebDriver driver;
@@ -17,8 +19,15 @@ public class Program01 {
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
 		
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+		
+		wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.name("field-keywords"))));
+		
 		Select dropdown = new Select(driver.findElement(By.id("searchDropdownBox")));
 		dropdown.selectByVisibleText("Books");	
+		
+		
 		driver.findElement(By.name("field-keywords")).sendKeys("Java Books"+Keys.ENTER);
 	
 	}
